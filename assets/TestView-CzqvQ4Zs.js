0f127bde321b7905f02bd54f89b1d869e4b0d16b
DukeOfCheese <81943364+DukeOfCheese@users.noreply.github.com>
2024-02-23 20:54:21 +1100

    enjoy

---
+++ assets/TestView-CzqvQ4Zs.js
@@ -0,0 +1,2 @@
+import{r as o,j as e}from"./vendor-CtbUqpA0.js";import{B as i}from"./index-CXsI4q_W.js";import"./auth-72H9TcpW.js";import"./caption-parsing-BGqd6Hpv.js";import"./react-dom-BnEYDcZu.js";import"./locales-BNmjpJ3z.js";import"./Icons-XXm5y-G7.js";import"./language-db-C33ehKO5.js";import"./hls-Di-l_7QN.js";function x(){const[r,t]=o.useState(!1);if(r)throw new Error("I crashed");return e.jsx(i,{onClick:()=>t(!0),children:"Crash me!"})}export{x as default};
+//# sourceMappingURL=TestView-CzqvQ4Zs.js.map
---
+++ config.js
@@ -0,0 +1,19 @@
+window.__CONFIG__ = {
+  // The URL for the CORS proxy, the URL must NOT end with a slash!
+  VITE_CORS_PROXY_URL: "https://tubular-creponne-e839ea.netlify.app/",
+
+  // The READ API key to access TMDB
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMDc4MjdiMWIzM2Y5ZWJlMWQ3ZjI0ODIwZWYyM2UzNSIsInN1YiI6IjY1ZDg2OGVmOTkyNTljMDE3Yjk0NDk3NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.oYZEAlvjuSiujwCPd56KkX8Ih1sllhLwb3O0aQnN6Ak",
+
+  // The DMCA email displayed in the footer, null to hide the DMCA link
+  VITE_DMCA_EMAIL: null,
+
+  // Whether to disable hash-based routing, leave this as false if you don't know what this is
+  VITE_NORMAL_ROUTER: false,
+
+  // The backend URL to communicate with, defaults to the movie-web hosted one at backend.movie-web.app
+  VITE_BACKEND_URL: null,
+
+  // A comma separated list of disallowed IDs in the case of a DMCA claim - in the format "series-<id>" and "movie-<id>"
+  VITE_DISALLOWED_IDS: ""
+};
