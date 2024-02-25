df7c121a94fec0caaa61ddc3943c0b2c6b22a028
DukeOfCheese <81943364+DukeOfCheese@users.noreply.github.com>
2024-02-25 21:31:26 +1100

    Update config.js

--- config.js
+++ config.js
@@ -6,13 +6,13 @@ window.__CONFIG__ = {
   VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMDc4MjdiMWIzM2Y5ZWJlMWQ3ZjI0ODIwZWYyM2UzNSIsInN1YiI6IjY1ZDg2OGVmOTkyNTljMDE3Yjk0NDk3NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.oYZEAlvjuSiujwCPd56KkX8Ih1sllhLwb3O0aQnN6Ak",
 
   // The DMCA email displayed in the footer, null to hide the DMCA link
-  VITE_DMCA_EMAIL: null,
+  VITE_DMCA_EMAIL: "dukeofcheesebusiness@gmail.com",
 
   // Whether to disable hash-based routing, leave this as false if you don't know what this is
   VITE_NORMAL_ROUTER: false,
 
   // The backend URL to communicate with, defaults to the movie-web hosted one at backend.movie-web.app
-  VITE_BACKEND_URL: null,
+  VITE_BACKEND_URL: "https://backend-production-7ddd.up.railway.app/",
 
   // A comma separated list of disallowed IDs in the case of a DMCA claim - in the format "series-<id>" and "movie-<id>"
   VITE_DISALLOWED_IDS: ""
