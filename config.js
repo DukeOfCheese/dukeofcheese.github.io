0ecc5de3b855a316c58b532a0c68cad0b1326529
DukeOfCheese <81943364+DukeOfCheese@users.noreply.github.com>
2024-04-02 15:37:29 +1100

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,20 @@
 window.__CONFIG__ = {
   // The URL for the CORS proxy, the URL must NOT end with a slash!
+  // If not specified, the onboarding will not allow a "default setup". The user will have to use the extension or set up a proxy themselves
   VITE_CORS_PROXY_URL: "https://tubular-creponne-e839ea.netlify.app",
 
   // The READ API key to access TMDB
   VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMDc4MjdiMWIzM2Y5ZWJlMWQ3ZjI0ODIwZWYyM2UzNSIsInN1YiI6IjY1ZDg2OGVmOTkyNTljMDE3Yjk0NDk3NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.oYZEAlvjuSiujwCPd56KkX8Ih1sllhLwb3O0aQnN6Ak",
 
   // The DMCA email displayed in the footer, null to hide the DMCA link
-  VITE_DMCA_EMAIL: "null",
+  VITE_DMCA_EMAIL: null,
 
   // Whether to disable hash-based routing, leave this as false if you don't know what this is
   VITE_NORMAL_ROUTER: false,
 
-  // The backend URL to communicate with, defaults to the movie-web hosted one at backend.movie-web.app
+  // The backend URL to communicate with
   VITE_BACKEND_URL: "https://mw-backend.lonelil.com",
 
   // A comma separated list of disallowed IDs in the case of a DMCA claim - in the format "series-<id>" and "movie-<id>"
-  VITE_DISALLOWED_IDS: ""
+  VITE_DISALLOWED_IDS: "",
 };
